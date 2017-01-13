6dff00c86c4e312324cd68f93da6a50d41d98ff8
thibault <thibault.daboville@gmail.com>
2017-01-13 16:10:52 +0100

    quasi-fini

--- js/app.js
+++ js/app.js
@@ -5,91 +5,94 @@ $(document).ready(function(){
     $(btn[i]).css("background", btnColor);
   }
 
-    var paint=false;
-    var clickX, clickY;
-    context = document.getElementById('paper').getContext("2d");
-
-    var color = "black";
-
-    $(':button').click(function(){
-      color = $(this).attr('data-color');
-    });   // Attribue la couleur du carré au pinceau
+  var paint=false;
+  var clickX, clickY;
+  canvas =document.getElementById('paper');
+  context = canvas.getContext('2d');
 
 
 
-    //bouton clear area
-    $('.clear').click(function(){
-        location.reload();
-    });
+  var color = "black";
+
+  $(':button').click(function(){
+    color = $(this).attr('data-color');
+      if($("#fondchange").is(':checked')){
+        $('canvas').css('background-color', color);
+      }
+  });   // Attribue la couleur du carré au pinceau
+
+
+  //bouton clear area
+  $('.clear').click(function(){
+    location.reload();
+  });
 
 
 
-//définit la fonction "dessiner en fonction des coordonnées de la souris"
-$('#paper').mousedown(function(e){
-  paint = true;
-  redraw(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, false);
+  //définit la fonction "dessiner en fonction des coordonnées de la souris"
+  $('#paper').mousedown(function(e){
+    paint = true;
+    redraw(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, false);
 
-});
+  });
 
-$('#paper').mousemove(function(e){
-  if(paint){
-    redraw(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+  $('#paper').mousemove(function(e){
+    if(paint){
+      redraw(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+    }
+  });
+
+  $('#paper').mouseup(function(e){
+    paint = false;
+  });
+
+  $('#paper').mouseleave(function(e){
+    paint = false;
+  });
+
+  var largeur = "5";
+  $('.val').click(function(){
+    largeur = $(this).attr("data");
+  });
+
+  // définit la fonction redraw
+  function redraw(x, y, dragging){
+    if(dragging) {
+      context.beginPath();
+      context.strokeStyle=color;
+      context.lineWidth=largeur;
+      context.lineJoin="round";
+      context.moveTo(clickX, clickY);
+      context.lineTo(x, y);
+      context.closePath();
+      context.stroke();
+    }
+    clickX=x;
+    clickY=y;
+
   }
-});
 
-$('#paper').mouseup(function(e){
-  paint = false;
-});
+  //création de la gomme
+  $('.gomme').click(function(){
+    if(dragging) {
+      context.beginPath();
+      context.strokeStyle=color;
+      context.lineWidth="5";
+      context.lineJoin="round";
+      context.moveTo(clickX, clickY);
+      context.lineTo(x, y);
+      context.closePath();
+      context.stroke();
+    }
+    clickX=x;
+    clickY=y;
+    console.log('.gomme');
+  });
 
-$('#paper').mouseleave(function(e){
-  paint = false;
-});
-
-var largeur = "5";
-$('.val').click(function(){
-  largeur = $(this).attr("data");
-});
-
-// définit la fonction redraw
-function redraw(x, y, dragging){
-  if(dragging) {
-    context.beginPath();
-    context.strokeStyle=color;
-    context.lineWidth=largeur;
-    context.lineJoin="round";
-    context.moveTo(clickX, clickY);
-    context.lineTo(x, y);
-    context.closePath();
-    context.stroke();
-  }
-  clickX=x;
-  clickY=y;
-
-}
-
-//création de la gomme
-$('.gomme').click(function(){
-  if(dragging) {
-    context.beginPath();
-    context.strokeStyle=color;
-    context.lineWidth="5";
-    context.lineJoin="round";
-    context.moveTo(clickX, clickY);
-    context.lineTo(x, y);
-    context.closePath();
-    context.stroke();
-  }
-  clickX=x;
-  clickY=y;
-  console.log('.gomme');
-});
-
-// changer le background
-if (('background').is(checked)){
-$('background-color')  === ':button data-color';
-} else{
-
-}
+  // changer le background
+  // $('input:checkbox').is(':checked')(fuction(){
+  //   $('paper')  === ':button data-color';
+  // });
 
 });
 
